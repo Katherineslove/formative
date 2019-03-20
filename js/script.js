a09c79d4a668118ad2d2bbdbf01ab5b2c3fc091b
Katherine Love <270010932@bun2-206-16.local>
2019-03-21 11:40:48 +1300

    Responsive Tablet

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function showNav(){
-    var menu = document.getElementById("list");
+    var menu = document.getElementById("yeet");
     if(menu.style.display === 'block'){
         menu.style.display = 'none';
     } else {
